f6ed1322ac29eaebb090c0a1c41ab4b7f54ad2cb
Elizabeth Ucar <elizg77@hotmail.com>
2022-01-17 13:21:20 +0100

    uppdaterad frontend

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-document.addEventListener("submit" , (event=>{
-    console.log("Submitted")
+document.addEventListener("submit" , (event)=>{
+    console.log("Validating")
+    let user = document.getElementById("user").value;
+    let password = document.getElementById("password").value;
+
+    let request = new XMLHttpRequest();
+    request.open("POST", "http://localhost:3000/authorize");
+    //request.send(new FormData(formElement));
+    console.log("Validated")
+
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.send(`user=${user}&password=${password}`);
+
     event.preventDefault()
 })
-
-.fetch("http://localhost:3000/" , {})
-.then(()=>{})
-.catch(()=>{
-    console.log("an error occured")
-}))
+ 
